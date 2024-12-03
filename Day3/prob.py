42e1f97b704eed85ee94d1fa61b707aadaffcd8b
Landen Bailey <54867037+landiinii@users.noreply.github.com>
2024-12-03 08:48:06 -0700

    Day 3

--- Day3/prob.py
+++ Day3/prob.py
@@ -1,5 +1,20 @@
-with open('test.txt', 'r') as file:
-    input = file.read()
-    
-input = input.split('\n')
+import re
 
+
+with open('input.txt', 'r') as file:
+    input = file.read()
+
+dos = input.split('do()')
+
+matches = []
+for do in dos:
+    doit = do.split('don\'t()')
+    matches += re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', doit[0])
+
+
+total = 0
+for match in matches:
+    nums = match[4:-1].split(',')
+    total += int(nums[0]) * int(nums[1])
+
+print(total)
